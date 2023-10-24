6d8f477c488c0132b4430da7138897428b99f89d
joe <joseph.satow@gmail.com>
2023-10-24 13:56:19 +0000

    rearrange structure

--- robinReader/main.py
+++ robinReader/main.py
@@ -1,10 +1,10 @@
-from helper_funcs.fileFunctions import getData
-from helper_funcs.filter import filterData
-from helper_funcs.dataFunctions import getCurrentValue
-from helper_funcs.contractDictFunctions import createNewDictEntry
-from helper_funcs.contractDictUpdate import getContractDictUpdate
-from helper_funcs.tradeListFunctions import getTradeDictUpdate
-from helper_funcs.outputFunctions import writeCSV
+from .fileFunctions import getData
+from .filter import filterData
+from .dataFunctions import getCurrentValue
+from .contractDictFunctions import createNewDictEntry
+from .contractDictUpdate import getContractDictUpdate
+from .tradeListFunctions import getTradeDictUpdate
+from .outputFunctions import writeCSV
      
 # Get account activity list
 accountActivityList = getData()
---
+++ tests/run.py
@@ -0,0 +1,5 @@
+def main():
+    print("sdf")
+
+if __name__ == "__main__":
+    main()
